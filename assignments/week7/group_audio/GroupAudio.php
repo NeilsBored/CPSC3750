98f0c71e1f2bc0459e0f9e4b5b021804a5b66654
Shane John <144292740+NeilsBored@users.noreply.github.com>
2025-07-01 16:50:14 -0400

    Seemed to work on hosted server, so I commented the file and am going to see if I can run with common nav working this time.

--- assignments/week7/group_audio/GroupAudio.php
+++ assignments/week7/group_audio/GroupAudio.php
@@ -3,8 +3,8 @@
   Author: Shane John
   Date: 06-30-25
   Course: CPSC 3750 – Web Application Development
-  Purpose: 
-  Notes:
+  Purpose: A showcase of a simple audio control element, along with individual controls for volume and track time control
+  Notes: I had to change the format to PHP to get the common nav to port in correctly, also I am scared of the project version of this assignment :(
 -->
 
 <!DOCTYPE html>
@@ -13,6 +13,8 @@
   <title>Group: Audio</title>
   <script src="../jquery-3.7.1.min.js"></script>  
    <link rel="stylesheet" href="../../../main_style.css">
+
+   <!-- Internal Styling -->
   <style>
     /* Main div */
     div.music 
@@ -41,10 +43,12 @@
       font-weight: bold; 
       padding-bottom: 5px;
     }
+    /* Time Slider */
     #timeRange 
     {
       width: 250px;
     }
+    /* Playback Information */
     .playback
     {
       margin-top: 30px;
@@ -54,44 +58,35 @@
 
 </head>
 <body>
-  
- <nav style="top:0px; width:100%;">
-    <div class="content">
-    <div class="logo"><a href="../../../index.php" style="padding-left:10px;">ShaneJohn.com</a></div>
-        <ul class="links">
-            <li><a href="../../../index.php">Home</a></li>
-            <li><a href="https://github.com/NeilsBored/CPSC3750">Github Link</a></li>
-            <li><a href="mailto:shanej@clemson.edu">Send Feedback</a></li>
-        </ul>
-    </div>
-    </div>
-</nav>
-
-
+  <!--Common NavBar-->
+  <div class="wrapper" style="border-radius: 20px;">
+    <?php include '../../../navbar.php'; ?>
+  </div>
+  <!--Cool Page Title-->
   <h1 class="music">HTML DOM Audio Object Sampler</h1>
-
+ 
   <div class="music">
+    <!-- Audio Element -->
     <div>
-      <!-- Audio Element -->
       <label>Audio Element</label>
       <p class="music"><i>Spiritual</i> - John Coltrane </p>
       <audio id="myAudio" controls preload="metadata"> 
         <source src="Spiritual.mp3" type="audio/mpeg">
       </audio>
     </div>
-
-<div>
-    <div class="playback">
-      <label>Current Time</label>
-      <span id="currentTime">–</span> seconds 
+    <!-- Audio Info Display -->
+    <div>
+      <div class="playback">
+        <label>Current Time</label>
+        <span id="currentTime">–</span> seconds 
+      </div>
+      <div class="playback">
+        <label>Full Track Length</label>
+        <span id="duration">–</span> seconds
+      </div> 
     </div>
-    <div class="playback">
-      <label>Full Track Length</label>
-      <span id="duration">–</span> seconds
-    </div> 
-  </div>
-
-   <div>
+    <!-- Playback Controls -->
+    <div>
       <!-- Play / Pause -->
       <div class="control">
         <label>Play / Pause Control</label>
@@ -104,18 +99,18 @@
         <input id="volumeRange" type="range" min="0" max="1" step="0.05" value="1">
         <span id="volumeValue">100%</span>
       </div>
-
+      <!-- Time Changer-->
       <div class="control">
         <label>Playback Slider</label>
         <input type="range" id="timeRange" min="0" max="0" step="0.01" value="0">
       </div>
-
   </div>
-  
 
+  <!-- Internal js(with jQuery) -->
   <script>
     $(document).ready(function()
       {
+        // Element Variables
         var $audio = $('#myAudio'),
             audio = $audio[0],
             $playButton = $('#playButton'),
@@ -125,21 +120,22 @@
             $timeRange = $('#timeRange'),
             $currentTime = $('#currentTime'),
             $duration = $('#duration');
-
+        // Play/Pause Click Event
         $playButton.click(function() {audio.play();});
         $pauseButton.click(function() {audio.pause();});
-
+        // Volume Change Event
         $audio.on('volumechange', function() 
         {
           $volume.text((audio.volume * 100).toFixed(0) + '%');
         });
         $volumeRange.on('input', function() {audio.volume = this.value;});
-      
+        // Metadata Grab for Display
         $audio.on('loadedmetadata', function() 
         {
           $duration.text(audio.duration.toFixed(0));
           $timeRange.attr({max: audio.duration, step: 0.01 }).val(0);
         });
+        // Playback Time Update
         $audio.on('timeupdate', function() 
         {
           $currentTime.text(audio.currentTime.toFixed(0));
